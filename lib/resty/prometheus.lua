e5e56d301ef92dafb56a72879e8ab912662bfc78
tzssangglass <tzssangglass@gmail.com>
2023-03-21 04:33:45 +0800

    counter
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- lib/resty/prometheus.lua
+++ lib/resty/prometheus.lua
@@ -1,35 +1,53 @@
-local lmdb_txn = require "resty.lmdb.transaction"
-local txn = require("resty.lmdb.transaction")
-local lmdb = require("resty.lmdb")
+local lmdb_txn          = require "resty.lmdb.transaction"
+local txn               = require("resty.lmdb.transaction")
+local lmdb              = require("resty.lmdb")
+local ngx_get_phase     = ngx.get_phase
+local new_tab           = require "table.new"
+local type              = type
+local tostring          = tostring
+local tonumber          = tonumber
 
-local type = type
-local tostring = tostring
+-- copy form https://github.com/knyar/nginx-lua-prometheus/blob/main/prometheus.lua#L73-L87
+local TYPE_COUNTER    = 0x1
+local TYPE_GAUGE      = 0x2
+local TYPE_HISTOGRAM  = 0x4
+local TYPE_LITERAL = {
+    [TYPE_COUNTER]   = "counter",
+    [TYPE_GAUGE]     = "gauge",
+    [TYPE_HISTOGRAM] = "histogram",
+}
 
 local _M = {}
+local mt = { __index = _M }
 
+function _M.init()
+    if ngx_get_phase() ~= "init_worker" then
+        return nil, "only can be called in init_worker phase"
+    end
 
-function _M.init_worker()
     local t = lmdb_txn.begin(1)
     t:db_open(true)
     local ok, err = t:commit()
     if not ok then
         return nil, "failed to create and open the LMDB database, err: " .. err
     end
+
+    local self = setmetatable({}, mt)
+    self.registry = new_tab(8, 0)
+    self.initialized = true
+    return self
 end
 
 
-function _M.counter(key, value)
-    if not key then
-        return nil, "key is required"
-    end
-
+local function inc(key)
+    local value = lmdb.get(key)
     if not value then
-        return nil, "value is required"
+        value = 0
     end
 
-    if type(value) ~= "number" then
-        return nil, "value must be a number"
-    end
+    value = tonumber(value)
+
+    value = value + 1
 
     value = tostring(value)
 
@@ -42,8 +60,29 @@ function _M.counter(key, value)
     end
 end
 
+function _M:counter(name, help, labels)
+    if not name then
+        return nil, "name is required"
+    end
 
-function _M.collect(key)
+    if not labels then
+        return nil, "labels is required"
+    end
+
+    local metric = new_tab(0, 8)
+    metric.name = name
+    metric.help = help
+    metric.labels = labels
+    metric.type = TYPE_COUNTER
+    metric.inc = inc
+
+    self.registry[name] = metric
+    return metric
+end
+
+
+function _M:collect(key)
+    ngx.header.content_type = "text/plain"
     local value = lmdb.get(key)
     return value
 end
